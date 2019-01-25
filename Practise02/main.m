9005d2a03eac0cc739fbcdfeebecd453285898a9
Rui Cao <rayray199085@gmail.com>
2019-01-25 15:11:02 +1100

    Initial Commit

---
+++ Practise02/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Practise02
+//
+//  Created by Stephen Cao on 25/1/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
